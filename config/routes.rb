331020bc7149a621288cb16810d4700628df80a5
Kazuki.M <kazuki0922m@gmail.com>
2022-12-21 22:26:37 +0900

    コミット

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'home#top'
   resources :users
   get '/check' => 'users#check'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/login"  => "sessions#new"
+  post 'login', to: "sessions#create"
+
 end
